04a0c9eee10a7c5b433b718f3a4d259d5282556b
Hrrsmjd <hrrsmjd@gmail.com>
2021-09-26 13:49:39 +0100

    endpoint models

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-j)h*lxg5xf4*1q^4wkmu-apzf1=955ln6zzgz-nrd5^a%7r=&s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    # apps
+    'apps.endpoints'
 ]
 
 MIDDLEWARE = [
