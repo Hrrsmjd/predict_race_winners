b7f7218f6fc3f204f24892035f066bb801d25840
Hrrsmjd <hrrsmjd@gmail.com>
2021-09-28 15:28:41 +0100

    Add predict view..

--- backend/apps/endpoints/tests.py
+++ backend/apps/endpoints/tests.py
@@ -1,3 +1,34 @@
+from django.http import response
 from django.test import TestCase
 
 # Create your tests here.
+from rest_framework.test import APIClient
+
+class EndpointTests(TestCase):
+    def test_predict_view(self):
+        client = APIClient()
+        input_data = [
+            {
+                "raceID": 1,
+                "horseID": 1,
+                "weight": 100,
+                "ageInDays": 1000,
+                "daysSinceLastRace": 10,
+                "nPastRaces": 10 
+            },
+            {
+                "raceID": 1,
+                "horseID": 2,
+                "weight": 200,
+                "ageInDays": 5000,
+                "daysSinceLastRace": 1000,
+                "nPastRaces": 1
+            }
+        ]
+        classifier_url = '/api/v1/predict_race_winners/predict'
+        response = client.post(classifier_url, input_data, format='json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['raceID'], 1)
+        self.assertEqual(response.data['horseID'], 1)
+        self.assertTrue('request_id' in response.data)
+        self.assertTrue('status in response.data')
--- backend/apps/endpoints/views.py
+++ backend/apps/endpoints/views.py
@@ -1,8 +1,13 @@
+from django.http import response
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework import viewsets
-from rest_framework import mixins
+import json
+
+from numpy.random import rand
+
+from rest_framework import mixins, status, views, viewsets
+from rest_framework.response import Response
 
 from apps.endpoints.models import Endpoint
 from apps.endpoints.serializers import EndpointSerializer
@@ -15,6 +20,9 @@ from apps.endpoints.serializers import MLAlgorithmStatusSerializer
 
 from apps.endpoints.models import MLRequest
 from apps.endpoints.serializers import MLRequestSerializer
+
+from apps.ml.registry import MLRegistry
+from server.wsgi import registry
 
 
 class EndpointViewSet(
@@ -62,3 +70,45 @@ class MLRequestViewSet(
 ):
     serializer_class = MLRequestSerializer
     queryset = MLRequest.objects.all()
+
+
+class PredictView(views.APIView):
+    def post(self, request, endpoint_name, format=None):
+
+        algorithm_status = self.request.query_params.get('status', 'Production')
+        algorithm_version = self.request.query_params.get('version')
+        
+        algs = MLAlgorithm.objects.filter(parent_endpoint__name=endpoint_name, 
+                                          status__status=algorithm_status,
+                                          status__active=True)
+
+        if algorithm_version is not None:
+            algs = algs.filter(version=algorithm_version)
+
+        if len(algs) == 0:
+            return Response(
+                {'status': 'Error', 'message': 'ML algorithm selection is ambiguous. Please specify algorithm version.'},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        # Index for A/B testing
+        alg_index = 0
+        if algorithm_status == 'ab_testing':
+            alg_index = 0 if rand() < 0.5 else 1
+        
+        algorithm_object = registry.endpoints[algs[alg_index].id]
+        prediction = algorithm_object.compute_prediction(request.data)
+
+        horseID = prediction['label'] if 'label' in prediction else 'error'
+        ml_request = MLRequest(
+            input_data=json.dumps(request.data),
+            full_response=prediction,
+            response=horseID,
+            feedback='',
+            parent_mlalgorithm=algs[alg_index],
+        )
+        ml_request.save()
+
+        prediction['request_id'] = ml_request.input_data
+
+        return Response(prediction)
+
