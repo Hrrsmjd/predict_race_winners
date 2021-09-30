7bc543de71ba56dd352b890a0043a530248df80e
Hrrsmjd <1096ha@gmail.com>
2021-09-30 14:14:05 +0100

    Update files...

--- frontend/src/apps/Notebook.js
+++ frontend/src/apps/Notebook.js
@@ -1,27 +1,12 @@
-import { useState } from 'react';
-import { Document, Page, pdfjs } from 'react-pdf';
 import '../App.css';
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 function Notebook() {
-  const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1);
-
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages);
-  }
-
   return (
     <div className="Notebook">
-      <Document 
-        file="test.pdf"
-        onLoadSuccess={onDocumentLoadSuccess}
-      >
-        <Page pageNumber={pageNumber}/>
-      </Document>
-      <p>Page {pageNumber} of {numPages}</p>
+      <h1> Hello Notebook </h1>
     </div>
   );
+
 }
 
 export default Notebook;
